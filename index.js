c1d1081c42cb23a440658116954019fc350a6b92
KhalilurRahmanKhan <khanjahid373@gmail.com>
2022-05-21 00:12:26 +0600

    connect mongodb atlas

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
-const express = require('express')
-const app = express()
-const port = 5000
+const express = require('express');
+const app = express();
+require('dotenv').config();
 
+
+const port = process.env.PORT || 5000 ;
+
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://process.env.DB_USER:process.env.DB_PASSWORD@bookdb.ruoxg.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  // client.close();
+});
 
 
 app.get('/', (req, res) => {
