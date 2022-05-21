a89b87de492104972dcb539330ec9a0425996180
Khalilur Rahman Khan <khanjahid373@gmail.com>
2022-05-21 12:34:49 +0600

    add view api

--- index.js
+++ index.js
@@ -1,26 +1,61 @@
 const express = require('express');
 const app = express();
 require('dotenv').config();
+const cors = require('cors');
+const { MongoClient, ServerApiVersion,ObjectId } = require('mongodb');
+const { isObjectIdOrHexString } = require('mongoose');
 
 
 const port = process.env.PORT || 5000 ;
 
 
-
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://process.env.DB_USER:process.env.DB_PASSWORD@bookdb.ruoxg.mongodb.net/?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  // client.close();
-});
+app.use(cors())
+app.use(express.json());
 
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-  })
+const uri = "mongodb+srv://Khalilur:khan373@bookdb.ruoxg.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true,serverApi: ServerApiVersion.v1 });
+
+
+
+
+async function run(){
+  try {
+      await client.connect();
+
+      const bookDB = client.db("bookDB");
+      const inventory = bookDB.collection("inventory"); 
+
+
+
+      app.get('/inventory', async (req, res) => {
+      const query = req.query;
+
+      const cursor = inventory.find(query);
+
+      const result = await cursor.toArray();
+
+      res.send(result);
+      });
+    
   
-  app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-  })
+
+
+  } catch (e) {
+      console.error(e);
+  } finally {
+      // await client.close();
+  }
+}
+
+run().catch(console.error);
+
+
+
+    
+ 
+
+  
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+})
